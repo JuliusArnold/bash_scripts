239007ce0a9302ae3396cb231a7ea8dcc5cb1895
JuliusArnold <julius.arnold@hotmail.com>
2022-06-28 10:33:48 +0200

    Update reshape.sh adding reshape 1900x1080

--- reshape.sh
+++ reshape.sh
@@ -13,7 +13,8 @@ main()
 choice=$(echo -e "hd
 mun
 laptop
-dkfz" | dmenu -i -p 'Reshape Location: ')
+dkfz
+auto" | dmenu -i -p 'Reshape Location: ')
 echo $choice
 
 if [ $choice == hd ]; then
@@ -24,41 +25,64 @@ elif [ $choice == mun ]; then
  	reshape_mun
 elif [ $choice == laptop ]; then
 	reshape_laptop
-elif [ $choice == laptop ]; then
-	echo res	
+elif [ $choice == dkfz ]; then
+	reshape_dkfz	
+elif [ $choice == auto ]; then
+	reshape_auto
 fi
 }
 
-
-reshape_hd2()
+reshape_auto()
 {
 	xrandr --auto
-	xrandr --output HDMI-1 --left-of eDP-1
-	xrandr --output HDMI-1 --scale 0.6x0.6
-	xrandr --output eDP-1 --scale 0.6x0.6
-
+}
+reshape_dkfz()
+{
+	xrandr --output eDP-1 --scale 1x1
+	xrandr --output DP-2 --scale 1x1
+	xrandr --output DP-2 --right-of eDP-1
+	xrandr --output eDP-1 --mode 1920x1080
+	#exec --no-starup-id i3-msg 'workspace 9; exec /opt/cisco/anyconnect/bin/vpnui'
 }
 
 
 reshape_hd()
  {
-	 xrandr --prop | grep "[^dis]connected" | cut --delimiter=" " -f1> "$HOME/currentScreens.txt"
-	 echo "Number of active screens:" $(< "$HOME/currentScreens.txt" wc -l)
-	 if [ $(< "$HOME/currentScreens.txt" wc -l) -gt 1 ]; then
-	    xrandr --auto && xrandr --output $(cat "$HOME/currentScreens.txt" | awk 'NR==1') --primary
-	    xrandr --auto && xrandr --output $(cat "$HOME/currentScreens.txt" | awk 'NR==2') --right-of $(cat "$HOM    E/currentScreens.txt" | awk 'NR==1')
-	 else   
-	    echo "Nothing to do"
-	 fi
-	 xrandr --output HDMI-1 --left-of eDP-1
-	 reshape_laptop
-	 rm "$HOME/currentScreens.txt"
+	xrandr --output eDP-1 --scale 1x1
+	xrandr --output HDMI-1 --scale 1x1
+	 #xrandr --prop | grep "[^dis]connected" | cut --delimiter=" " -f1> "$HOME/currentScreens.txt"
+	 #echo "Number of active screens:" $(< "$HOME/currentScreens.txt" wc -l)
+	 #if [ $(< "$HOME/currentScreens.txt" wc -l) -gt 1 ]; then
+	 #   xrandr --auto && xrandr --output $(cat "$HOME/currentScreens.txt" | awk 'NR==1') --primary
+	 #   xrandr --auto && xrandr --output $(cat "$HOME/currentScreens.txt" | awk 'NR==2') --right-of $(cat "$HOM    E/currentScreens.txt" | awk 'NR==1')
+	 #else   
+	 #   echo "Nothing to do"
+	 #fi
+	xrandr --output HDMI-1 --left-of eDP-1
+	xrandr --output eDP-1 --scale 0.6x0.6
+	xrandr --output HDMI-1 --scale 1x1
+	 #reshape_laptop
+	 #rm "$HOME/currentScreens.txt"
  }
 
 
 reshape_laptop()
 {
-	xrandr --output eDP-1 --scale 0.6x0.6
+	choice2=$(echo -e "0.3
+0.4
+0.5
+0.6
+0.7
+0.8
+0.9
+1
+1920x1080" | dmenu -i -p 'Size of Laptop: ')
+	echo xrandr --output eDP-1 --scale ${choice2}x${choice2}
+	if [ $choice2 = 1920x1080 ]; then
+    xrandr --output eDP-1 --mode $choice2
+  else
+    xrandr --output eDP-1 --scale ${choice2}x${choice2}
+  fi
 }
 
 
@@ -68,7 +92,7 @@ reshape_mun()
 	xrandr --output HDMI-1 --rotate inverted
 	xrandr --output HDMI-1 --right-of eDP-1
 	xrandr --output HDMI-1 --scale 0.6x0.6
-	xrandr --output eDP-1 --scale 0.6x0.6
+	xrandr --output eDP-1 --mode 1920x1080
 }
 
 # ==================================================================================== #
